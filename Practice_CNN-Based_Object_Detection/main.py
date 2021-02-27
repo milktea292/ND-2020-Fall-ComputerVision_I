d93a8444e399a98a9d1c62ceed6bdb4d4062ee26
Eric <59555520+milktea292@users.noreply.github.com>
2021-02-27 02:09:00 -0500

    Add files via upload

---
+++ Practice_CNN-Based_Object_Detection/main.py
@@ -0,0 +1,43 @@
+import argparse
+
+from resnet_helper import *
+from vgg_helper import *
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--classifier', help='Classifier must be "resnet" or "vgg"')
+args = ap.parse_args()
+
+def camera_loop():
+    cap = cv2.VideoCapture(0)
+    while (True):
+        _, frame = cap.read()
+
+        action = cv2.waitKey(1)
+
+        if args.classifier == 'resnet':
+            # resnet prediction
+            frame = classify_resnet(frame)
+        elif args.classifier == 'vgg':
+            # VGG prediction
+            frame = classify_vgg(frame)
+        cv2.imshow('camera', frame)
+
+        if action == ord('q') or action == 27:
+            break
+
+        if action == ord('r'):
+            # resnet prediction
+            frame = classify_resnet(frame)
+            cv2.imshow('ResNet', frame)
+        if action == ord('v'):
+            # vgg prediction
+            frame = classify_vgg(frame)
+            cv2.imshow('VGG', frame)
+
+    cap.release()
+
+
+if __name__ == '__main__':
+
+    camera_loop()
+    cv2.destroyAllWindows()
